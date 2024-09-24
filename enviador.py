948ef1bf08116fe6c04e52018c8af3d9298a518a
lizard020 <fernando.riesco1230@gmail.com>
2024-09-24 11:27:37 -0300

    enviador.py

--- enviador.py
+++ enviador.py
@@ -10,31 +10,39 @@ df = pd.read_excel(caminho)
 outlook = win32.Dispatch('outlook.application')
 
 print("Dados carregados.")
-msg = input("O que deseja fazer? ")
 
-if msg == 'visualizar':
-    print(df)
+while True:
+    msg = input("O que deseja fazer? (visualizar, enviar ou fechar) ")
 
-elif msg == 'enviar':
-    # Loop para cada linha da planilha
-    for index, row in df.iterrows():
-        # Criar o e-mail
-        mail = outlook.CreateItem(0)
-        
-        # Definindo o destinatário
-        mail.To = row['Email']
-        
-        # Definindo o assunto
-        mail.Subject = f"Proposta de Acordo - {row['Processo']} - {row['Reclamante']}"
-        
-        # Definindo o corpo do e-mail
-        mail.Body = (f"Sou o Fernando do time de acordos. Quero apresentar essa proposta:\n"
-                    f"\n"
-                    f"Nº do Processo: {row['Processo']}\n"
-                    f"Reclamante: {row['Reclamante']}\n"
-                    f"Valor do acordo: {row['Valor']}\n"
-                    f"\n"
-                    "Aguardo retorno.")
-        
-        # Exibir o e-mail (sem enviar)
-        mail.Display()
+    if msg == 'visualizar':
+        print(df)
+
+    elif msg == 'enviar':
+        # Loop para cada linha da planilha
+        for index, row in df.iterrows():
+            # Criar o e-mail
+            mail = outlook.CreateItem(0)
+            
+            # Definindo o destinatário
+            mail.To = row['Email']
+            
+            # Definindo o assunto
+            mail.Subject = f"Proposta de Acordo - {row['Processo']} - {row['Reclamante']}"
+            
+            # Definindo o corpo do e-mail
+            mail.Body = (f"Sou o Fernando do time de acordos. Quero apresentar essa proposta:\n"
+                        f"\n"
+                        f"Nº do Processo: {row['Processo']}\n"
+                        f"Reclamante: {row['Reclamante']}\n"
+                        f"Valor do acordo: {row['Valor']}\n"
+                        f"\n"
+                        "Aguardo retorno.")
+            
+            # Exibir o e-mail (sem enviar)
+            mail.Display()
+
+    elif msg == 'fechar':
+        break  # Encerra o loop
+
+    else:
+        print("Comando inválido. Insira: visualizar, enviar ou fechar.")
